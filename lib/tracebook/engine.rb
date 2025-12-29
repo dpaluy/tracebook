132a0206ac0a6e178167ed8fae5c85304e0891e9
dpaluy <dpaluy@users.noreply.github.com>
2025-12-29 10:55:53 -0500

    fix: remove duplicate migration initializer causing CI failure

--- lib/tracebook/engine.rb
+++ lib/tracebook/engine.rb
@@ -1,11 +1,5 @@
 module Tracebook
   class Engine < ::Rails::Engine
     isolate_namespace Tracebook
-
-    initializer "tracebook.migrations" do |app|
-      config.paths["db/migrate"].expanded.each do |path|
-        app.config.paths["db/migrate"] << path
-      end
-    end
   end
 end
